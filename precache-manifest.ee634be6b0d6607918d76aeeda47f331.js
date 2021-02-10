c88931f562c73d49bc03162c2ab89cd80a8539a5
tomaxo14 <tomek.przybylski@onet.pl>
2021-02-10 19:52:48 +0100

    Updates

---
+++ precache-manifest.ee634be6b0d6607918d76aeeda47f331.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d04031b96180561d049edb91ea21b4e",
+    "url": "/SkiResorts_Frontend/index.html"
+  },
+  {
+    "revision": "1334f39154033739ac9f",
+    "url": "/SkiResorts_Frontend/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "35c46e58c23fbc37d2ad",
+    "url": "/SkiResorts_Frontend/static/css/main.7326f8e5.chunk.css"
+  },
+  {
+    "revision": "1334f39154033739ac9f",
+    "url": "/SkiResorts_Frontend/static/js/2.fc4ad448.chunk.js"
+  },
+  {
+    "revision": "cf1c5bca3904ea249e99181b6e2372cf",
+    "url": "/SkiResorts_Frontend/static/js/2.fc4ad448.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35c46e58c23fbc37d2ad",
+    "url": "/SkiResorts_Frontend/static/js/main.ef5f23d0.chunk.js"
+  },
+  {
+    "revision": "fd1f4a8ef3a56f229267",
+    "url": "/SkiResorts_Frontend/static/js/runtime-main.2a06bb3e.js"
+  },
+  {
+    "revision": "bf0160d53c59b13c23b081063dd5709c",
+    "url": "/SkiResorts_Frontend/static/media/gondola.bf0160d5.png"
+  },
+  {
+    "revision": "cb320281eaa2e91252f05a04160dae6a",
+    "url": "/SkiResorts_Frontend/static/media/kanapa.cb320281.png"
+  },
+  {
+    "revision": "5099f48d6acc8b597a165cc546b60396",
+    "url": "/SkiResorts_Frontend/static/media/slide1.5099f48d.jpg"
+  },
+  {
+    "revision": "f53de4739f45fee9e787f1a3f8c63074",
+    "url": "/SkiResorts_Frontend/static/media/slide2.f53de473.jpg"
+  },
+  {
+    "revision": "98603d9ee2ab9ae60a108f996d3e052e",
+    "url": "/SkiResorts_Frontend/static/media/slide3.98603d9e.jpg"
+  },
+  {
+    "revision": "c9309e9ab325e8b1af09584b61781a19",
+    "url": "/SkiResorts_Frontend/static/media/talerzyk.c9309e9a.png"
+  }
+]);
