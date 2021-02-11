db30cf4dbda9d0cf07d29e11e6b03423823b2adf
tomaxo14 <tomek.przybylski@onet.pl>
2021-02-11 17:54:58 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/SkiResorts_Frontend/precache-manifest.d33d398a7ff90231904df10a82db455d.js"
+  "/SkiResorts_Frontend/precache-manifest.64a7dc93c07681c7d9d24607ded36f31.js"
 );
 
 self.addEventListener('message', (event) => {
